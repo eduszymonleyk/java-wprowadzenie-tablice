eb4023bfaa1c7ae18fc83c3ae82bd875133ab76a
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-14 20:12:09 +0100

    #7 przygotowanie klasy dla sortowania tablicy

---
+++ SortArray.java
@@ -0,0 +1,12 @@
+class SortArray {
+	public static void main(String args[]){
+		int[] numbers = {1, 5, 3, 2, 1};
+		
+		// wyświetl liczby
+		System.out.println(numbers[0]);
+		System.out.println(numbers[1]);
+		System.out.println(numbers[2]);
+		System.out.println(numbers[3]);
+		System.out.println(numbers[4]);
+	}
+}
