86649c6710709d34005b33cca866e56e68529ff4
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-14 21:05:40 +0100

    #11 wyświetlenie tablicy dwuwymiarowej

--- Lighthouses.java
+++ Lighthouses.java
@@ -23,12 +23,19 @@ class Lighthouses {
 		tab[2][2] = -1;
 		tab[2][3] = 0;
 		
-		tab[2][0] = -1;
-		tab[2][1] = -1;
-		tab[2][2] = -1;
-		tab[2][3] = -1;
+		tab[3][0] = -1;
+		tab[3][1] = -1;
+		tab[3][2] = -1;
+		tab[3][3] = -1;
 		
+		// wyświtlanie
+		// jeżeli jest -1, to wyświetl " "
+		// jeżeli jest -2, to wyświetl "X"
+		// w przeciwnym wypadku wyświetl co 
 		
-		
+		System.out.println(tab[0][0] +" "+tab[0][1] +" "+tab[0][2] + " " +tab[0][3]);
+		System.out.println(tab[1][0] +" "+tab[1][1] +" "+tab[1][2] + " " +tab[1][3]);
+		System.out.println(tab[2][0] +" "+tab[2][1] +" "+tab[2][2] + " " +tab[2][3]);
+		System.out.println(tab[3][0] +" "+tab[3][1] +" "+tab[3][2] + " " +tab[3][3]);
 	}
 }
