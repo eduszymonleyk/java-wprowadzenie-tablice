60ac6d39fd5d7215a470b7ea26e42626433c7bb4
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-14 21:00:34 +0100

    #11 wprowadzenie 4x4 lighthouses

---
+++ Lighthouses.java
@@ -0,0 +1,34 @@
+class Lighthouses {
+	public static void main(String args[]){
+		
+		int[][] tab = new int[4][4];
+		
+		// latarnia    -  liczba określa ile statków powinno być w zasięgu
+		// puste pole  -  -1
+		// statek      -  -2
+		
+		// wprowadzenie danych
+		tab[0][0] = -1;
+		tab[0][1] = -1;
+		tab[0][2] = -2;
+		tab[0][3] = -1;
+		
+		tab[1][0] = 1;
+		tab[1][1] = -1;
+		tab[1][2] = -1;
+		tab[1][3] = -1;
+		
+		tab[2][0] = -1;
+		tab[2][1] = -1;
+		tab[2][2] = -1;
+		tab[2][3] = 0;
+		
+		tab[2][0] = -1;
+		tab[2][1] = -1;
+		tab[2][2] = -1;
+		tab[2][3] = -1;
+		
+		
+		
+	}
+}
